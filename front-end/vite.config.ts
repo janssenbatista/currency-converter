2e21d9e2e8429823f182c8c7daf86fc4504a292f
Janssen Batista <batistajanssen.dev@gmail.com>
2026-03-30 11:50:43 -0300

    chore: add tailwindcss config

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -1,14 +1,10 @@
-/// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: "jsdom"
-  }
-})
+  plugins: [react(), tailwindcss()],
+});
